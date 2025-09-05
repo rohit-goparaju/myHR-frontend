0a6ad239bce4f05053555b5f4a9617d090f4f329
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2025-09-05 22:15:12 +0530

    add withcredentials to manage session, incorrect username password handling

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -1,17 +1,22 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "./App";
+import BackendAxios from "./BackendAxios";
 
 export default function Logout(){
     const navigate = useNavigate();    
     const {setUserValidity} = useUserContext();
     useEffect(
         ()=>{
-            localStorage.clear();
-            setUserValidity(false);
-            navigate("/", {replace : true});
+            BackendAxios.post("/Logout",{},{withCredentials: true}).then(
+                (res)=>{
+                    localStorage.clear();
+                    setUserValidity(false);
+                    navigate("/", {replace : true});
+                }
+            ).catch((err)=>console.error("Error: ", err))
         }
-        ,[]
+        ,[setUserValidity, navigate]
     );
     return null;
 }
